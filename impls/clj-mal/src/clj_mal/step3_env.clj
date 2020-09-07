4e76b6a433b128026b0365ccae9ad690ea9923b5
zachary <zachary@computer.local>
2020-09-07 12:25:42 -0500

    Some cleanup, step3 tests passing.

--- impls/clj-mal/src/clj_mal/step3_env.clj
+++ impls/clj-mal/src/clj_mal/step3_env.clj
@@ -5,71 +5,57 @@
             [clojure.repl :as cr])
   (:gen-class))
 
+(defn wrap-res [form env]
+  {:form form :env env})
+
 (def EVAL)
 
 (defn eval-coll [ast env coll-fn]
-  (->> ast
-       (map (comp :form #(EVAL % env)))
-       doall
-       coll-fn))
+  (wrap-res (->> ast
+                 (map (comp :form #(EVAL % env)))
+                 doall
+                 coll-fn)
+            env))
 
 (defn eval-ast [ast env]
   (cond
-    (symbol? ast) {:form (env/get-env env ast)
-                   :env env}
-    (seq? ast)    {:form (eval-coll ast env seq)
-                   :env env}
-    (vector? ast) {:form (eval-coll ast env vec)
-                   :env env}
-    (map? ast)    {:form (eval-coll (apply concat ast)
-                                    env
-                                    (partial apply hash-map))
-                   :env env}
-    :else         {:form ast
-                   :env env}))
+    (symbol? ast) (wrap-res (env/get-env env ast) env)
+    (seq? ast)    (eval-coll ast env seq)
+    (vector? ast) (eval-coll ast env vec)
+    (map? ast)    (eval-coll (apply concat ast)
+                             env
+                             (partial apply hash-map))
+    :else         (wrap-res ast env)))
+
+(defn eval-def [x1 x2 env]
+  (let [m (EVAL x2 env)]
+    (update m :env #(env/set-env % x1 (:form m)))))
+
+(defn eval-let [x1 x2 env]
+  (let [bindings (partition 2 x1)
+        new-env (reduce (fn [acc [k v]]
+                          (assoc acc k (:form (EVAL v acc))))
+                        (env/make-env env)
+                        bindings)]
+    (wrap-res (:form (EVAL x2 new-env)) env)))
 
 (defn READ [& [s]]
   (reader/read-str s))
-
-(defn update-env [{:keys [form env] :as m} k]
-  (println 111111111 m)
-  {:form form
-   :env (env/set-env env k form)})
-
-(defn eval-def [x1 x2 env]
-  (try
-    (-> x2
-        (EVAL env)
-        (update-env x1))
-    (catch Throwable e
-      (cr/pst e)
-      {:env env
-       :form nil})))
 
 (defn EVAL [ast env]
   (if-not (seq? ast)
     (eval-ast ast env)
     (let [[x0 x1 x2] ast]
       (cond
-        (empty? ast) {:form ast :env env}
+        (empty? ast) (wrap-res ast env)
         (= 'def! x0) (eval-def x1 x2 env)
-        (= 'let* x0) (let [bindings (partition 2 x1)
-                           new-env
-                           (reduce (fn [acc [k v]]
-                                     (assoc acc k (:form (EVAL v acc))))
-                                   (env/make-env env)
-                                   bindings)]
-                       {:form (:form (EVAL x2 new-env))
-                        :env env})
-        :else        (let [{:keys [form env]} (eval-ast ast env)]
-                       {:form (apply (first form) (rest form))
-                        :env env})))))
+        (= 'let* x0) (eval-let x1 x2 env)
+        :else        (let [m (eval-ast ast env)]
+                       (update m :form #(apply (first %)
+                                               (rest %))))))))
 
-#_(EVAL (READ "(let* (c 2) c)") repl-env)
-
-(defn PRINT [{:keys [form env]}]
-  {:form (printer/print-string form)
-   :env env})
+(defn PRINT [m]
+  (update m :form printer/print-string))
 
 (def repl-env
   {'+ +
@@ -83,11 +69,12 @@
         READ
         (EVAL env)
         PRINT)
-    (catch Throwable e (cr/pst e))))
+    (catch Throwable e (cr/pst e)
+           (wrap-res nil env))))
 
 ;;repl loop
 (defn repl-loop []
-  (loop [env repl-env]
+  (loop [env (env/make-env repl-env)]
     (println "user> ")
     (let [line (read-line)]
       (when line
